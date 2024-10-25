74291e343734ada6c65bf0f9ffddd0fb9f825b34
Sead Feng <seadfeng@gmail.com>
2024-10-25 19:40:36 +0800

    update api info

--- src/pages/api/info.ts
+++ src/pages/api/info.ts
@@ -3,6 +3,7 @@ export const runtime = "edge";
 
 export const GET: APIRoute = async () => {
   console.log("VITE_DIRECTUS_URL", import.meta.env.VITE_DIRECTUS_URL);
+  console.log("CF_VITE_DIRECTUS_URL", import.meta.env.CF_VITE_DIRECTUS_URL);
   return new Response(
     JSON.stringify({
       astro_url: import.meta.env.VITE_ASTRO_URL,
