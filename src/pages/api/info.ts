c772c106ef818734a794ebd338cac634aabdf092
Sead Feng <seadfeng@gmail.com>
2024-10-25 19:35:29 +0800

    add api info

---
+++ src/pages/api/info.ts
@@ -0,0 +1,22 @@
+import type { APIRoute } from "astro";
+export const runtime = "edge";
+
+export const GET: APIRoute = async () => {
+  console.log("VITE_DIRECTUS_URL", import.meta.env.VITE_DIRECTUS_URL);
+  return new Response(
+    JSON.stringify({
+      astro_url: import.meta.env.VITE_ASTRO_URL,
+    }),
+    {
+      status: 200,
+      headers: { "Content-Type": "application/json" },
+    }
+  );
+};
+
+export const ALL: APIRoute = () => {
+  return new Response(JSON.stringify({ error: "Method Not Allowed" }), {
+    status: 405,
+    headers: { "Content-Type": "application/json" },
+  });
+};
